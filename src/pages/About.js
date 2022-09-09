28ca9ebf43a9f7f9d700ae2e2129f8f3e02d9d67
DJ Mungin <103383531+OZ4Tech@users.noreply.github.com>
2022-09-09 16:17:09 -0400

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../pages/About.css"
+
+const About = () => {
+    return(
+        <><div>
+            <h2>About</h2>
+
+            <p>This App was created to give users the most up to date stock information, Bull or Bear Market! After signing up and signing in you'll find the information needed to determine Bull or Bear.</p>
+        </div><li>
+                <a href="../">Home</a>
+            </li></>
+    )
+}
+
+export default About;
